7a7b2ce6e2cbb1008e31ce1838beba2d8cc45367
Wanli Pu <puwanli@gmail.com>
2019-02-25 00:55:39 -0600

    update project

--- my_answers.py
+++ my_answers.py
@@ -20,7 +20,7 @@ class NeuralNetwork(object):
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
+        self.activation_function = lambda x : 1 / (1 + np.exp(-x)  # Replace 0 with your sigmoid calculation.
         
         ### If the lambda code above is not something you're familiar with,
         # You can uncomment out the following three lines and put your 
